9ef590d7a6000edb9b12d19f7440feae76d4a572
David M. Carr <david@carrclan.us>
2013-02-12 11:41:17 -0500

    tests: add coverage for private repo arg overrides

--- src/test/groovy/us/carrclan/david/gradle/bintray/BintrayRepositoriesExtensionSpec.groovy
+++ src/test/groovy/us/carrclan/david/gradle/bintray/BintrayRepositoriesExtensionSpec.groovy
@@ -55,18 +55,44 @@ class BintrayRepositoriesExtensionSpec extends Specification {
     }
 
     def "bintray.repo creates private bintray repository"() {
-        def repo
-
         when:
         project.with {
             repositories {
                 bintray.repo(repoOwner: 'davidmc24', repoName: 'gradle-bintray-plugin')
             }
         }
-        repo = project.repositories.BintrayDavidmc24GradleBintrayPlugin
+        def repo = project.repositories.BintrayDavidmc24GradleBintrayPlugin
 
         then:
         repo instanceof MavenArtifactRepository
         repo.url.toString() == 'http://dl.bintray.com/content/davidmc24/gradle-bintray-plugin'
+    }
+
+    def "bintray.repo allows specifying repository name"() {
+        when:
+        project.with {
+            repositories {
+                bintray.repo(name: 'MyRepo', repoOwner: 'davidmc24', repoName: 'gradle-bintray-plugin')
+            }
+        }
+        def repo = project.repositories.MyRepo
+
+        then:
+        repo instanceof MavenArtifactRepository
+        repo.url.toString() == 'http://dl.bintray.com/content/davidmc24/gradle-bintray-plugin'
+    }
+
+    def "bintray.repo allows specifying repository url"() {
+        when:
+        project.with {
+            repositories {
+                bintray.repo(url: 'http://mirrors.example.com/bintray/joe/maven', repoOwner: 'joe', repoName: 'maven')
+            }
+        }
+        def repo = project.repositories.BintrayJoeMaven
+
+        then:
+        repo instanceof MavenArtifactRepository
+        repo.url.toString() == 'http://mirrors.example.com/bintray/joe/maven'
     }
 }
